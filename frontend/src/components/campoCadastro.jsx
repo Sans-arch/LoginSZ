3e6a787af3f68ac449e0213a7a514d2c90bfff9a
Santiago Fleitas <69471715+Sans-arch@users.noreply.github.com>
2020-10-30 15:27:17 -0300

    Atualizando
    Atualizando repositório.

--- frontend/src/components/campoCadastro.jsx
+++ frontend/src/components/campoCadastro.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import App from '../main/App'
 import { connect } from 'react-redux'
@@ -8,67 +8,115 @@ import { bindActionCreators } from 'redux'
 import '../styles/campoCadastro.css'
 
 
-const jesus =  props =>   
-    (
-    <div className="login-form">
-        <h1 id="headerLogin">Cadastro</h1>
-        <form>
-            <label htmlFor="inputName" id="labelNome">Nome </label>
-            <input 
-                type="text"
-                id="inputName"
-                name="name"
-                required
-                placeholder="Cadastre o seu nome..."
-            />
-            <br></br>
-            <label htmlFor="inputPassword" id="labelSenha"> Senha </label>
-            <input
-                type="password"
-                id="inputPassword"
-                name="password"
-                required
-                placeholder="Cadastre sua senha..."
-            />
-            <br></br>
-            <label htmlFor="inputConfirmPassword" id="labelSenha"> Confirmar  </label>
-            <input
-                type="password"
-                id="inputConfirmPassword"
-                name="password"
-                required
-                placeholder="Confirme sua senha..."
-            />
-            <br></br>
-            <label htmlFor="inputEmail" id="labelEmail"> Email </label>
-            <input
-                type="text"
-                id="inputEmail"
-                name="email"
-                required
-                placeholder="Cadastre o seu email..."
-            />
-            <br></br>
-            <label htmlFor="inputTelefone" id="labelTelefone"> Telefone </label>
-            <input
-                type="text"
-                id="inputTelefone"
-                name="telefone"
-                required
-                placeholder="Cadastre o seu telefone..."
-            />
-            <br></br>
-            <button type="submit" id="enviar">Registrar</button>
-        </form>
-    </div>
-)
+export default class CampoCadastro extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: '',
+            password: '',
+            conferir: '',
+            email: '',
+            telefone: ''
+        }
+        this.verDigito = this.verDigito.bind(this)
+    }
 
-const mapDispatchToProps = dispatch => bindActionCreators ({
-    jesus
-}, dispatch)
+    verDigito(evento, name) {
+        if(name === 'name'){
+            this.setState({
+                name: evento.target.value
+            }) 
+        } else if(name === 'password') {
+            this.setState({
+                password: evento.target.value
+            })
+        } else if(name === 'conferir'){
+            this.setState({
+                conferir: evento.target.value
+            })
+        } else if(name === 'email'){
+            this.setState({
+                email: evento.target.value
+            })
+        } else if(name === 'telefone'){
+            this.setState({
+                telefone: evento.target.value
+            })
+        }
+    }
 
-const mapStateToProps = state => ({
-    neymar: state.cadastro.list
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(jesus)
+    render() {
+
+        console.log(this.state.name)
+        console.log(this.state.password)
+        console.log(this.state.conferir)
+        console.log(this.state.email)
+        console.log(this.state.telefone)
+
+
+        return (
+            <div className="login-form">
+                <h1 id="headerLogin">Cadastro</h1>
+                <form>
+                    <label htmlFor="inputName" id="labelNome">Nome </label>
+                    <input 
+                        type="text"
+                        id="inputName"
+                        name="name"
+                        value={this.state.name}
+                        onChange={(evento) => this.verDigito(evento, 'name')}
+                        required
+                        placeholder="Cadastre o seu nome..."
+                    />
+                    <br></br>
+                    <label htmlFor="inputPassword" id="labelSenha"> Senha </label>
+                    <input
+                        type="password"
+                        id="inputPassword"
+                        name="password"
+                        value={this.state.password}
+                        onChange={(evento) => this.verDigito(evento, 'password')}
+                        required
+                        placeholder="Cadastre sua senha..."
+                    />
+                    <br></br>
+                    <label htmlFor="inputConfirmPassword" id="labelSenha"> Confirmar  </label>
+                    <input
+                        type="password"
+                        id="inputConfirmPassword"
+                        name="conferir"
+                        value={this.state.conferir}
+                        onChange={(evento) => this.verDigito(evento, 'conferir')}
+                        required
+                        placeholder="Confirme sua senha..."
+                    />
+                    <br></br>
+                    <label htmlFor="inputEmail" id="labelEmail"> Email </label>
+                    <input
+                        type="text"
+                        id="inputEmail"
+                        name="email"
+                        value={this.state.email}
+                        onChange={(evento) => this.verDigito(evento, 'email')}
+                        required
+                        placeholder="Cadastre o seu email..."
+                    />
+                    <br></br>
+                    <label htmlFor="inputTelefone" id="labelTelefone"> Telefone </label>
+                    <input
+                        type="text"
+                        id="inputTelefone"
+                        name="telefone"
+                        value={this.state.telefone}
+                        onChange={(evento) => this.verDigito(evento, 'telefone')}
+                        required
+                        placeholder="Cadastre o seu telefone..."
+                    />
+                    <br></br>
+                    <button type="submit" id="enviar">Registrar</button>
+                </form>
+            </div>
+        )
+    }
+}  
--- frontend/src/components/campoLogin.jsx
+++ frontend/src/components/campoLogin.jsx
@@ -1,31 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 // Importando estilo
 import '../styles/campoLogin.css'
 
-export default props => (
-    <div className="login-form">
-        <h1 id="headerLogin">Login</h1>
-        <form>
-            <label htmlFor="inputName" id="labelNome">Nome </label>
-            <input 
-                type="text"
-                id="inputName"
-                name="name"
-                placeholder="Digite seu nome..."
-            />
-            <br></br>
-            <label htmlFor="inputPassword" id="labelSenha"> Senha </label>
-            <input
-                type="password"
-                id="inputPassword"
-                name="password"
-                placeholder="Digite sua senha..."
-            />
-            <br></br>
-            <button type="submit" id="enviar">Logar</button>
-        </form>
-    </div>
-)
+export default class CampoLogin extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.verDigito = this.verDigito.bind(this)
+        this.entrar = this.entrar.bind(this)
+    }
 
+    verDigito(evento, nome) {
+        if(nome === 'name'){
+            this.setState({
+                email: evento.target.value
+            }) 
+        } else if(nome === 'password') {
+            this.setState({
+                password: evento.target.value
+            })
+        }
+    }
+
+    entrar() {
+        if(this.state.email === 'neymar' && this.state.password === '1234' ){
+            alert('Parabéns, você foi logado com sucesso!')
+        } else {
+            alert('Usuário ou senha incorretos!')
+        }
+    }
+
+    render() {
+
+        console.log(this.state.email)
+        console.log(this.state.password)
+
+        return (
+            <div className="login-form">
+                <h1 id="headerLogin">Login</h1>
+                <form>
+                    <label htmlFor="inputName" id="labelNome">Nome </label>
+                    <input 
+                        type="text"
+                        id="inputName"
+                        name="name"
+                        value={this.state.email}
+                        onChange={(evento) => this.verDigito(evento, 'name')}
+                        placeholder="Digite seu nome..."
+                    />
+                    <br></br>
+                    <label htmlFor="inputPassword" id="labelSenha"> Senha </label>
+                    <input
+                        type="password"
+                        id="inputPassword"
+                        name="password"
+                        value={this.state.password}
+                        onChange={(evento) => this.verDigito(evento, 'password')}
+                        placeholder="Digite sua senha..."
+                    />
+                    <br></br>
+
+
+                    <button
+                     type="submit"
+                     onClick={this.entrar} 
+                     id="enviar">
+                         Logar
+                     </button>
+                </form>
+            </div>
+        )
+    }
+} 
